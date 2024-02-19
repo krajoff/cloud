6226728182e01d49695992ad111574d72f5b2caf
krajoff <t0k@bk.ru>
2024-02-19 15:51:48 +0300

    Slight changes for actuator and primetheus

--- item/src/main/java/com/example/items/ItemController.java
+++ item/src/main/java/com/example/items/ItemController.java
@@ -1,5 +1,6 @@
 package com.example.items;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ItemController {
     private final ItemService itemService;
 
+    private final MeterRegistry meterRegistry;
 
     @Autowired
-    public ItemController(ItemService itemService) {
+    public ItemController(ItemService itemService, MeterRegistry meterRegistry) {
         this.itemService = itemService;
+        this.meterRegistry = meterRegistry;
     }
 
     @GetMapping("/all")
     public String getAllItems(Model model) {
+        meterRegistry.counter("requests_to_items_all").increment();
         model.addAttribute("items", itemService.getAllItems());
         return "items";
     }
